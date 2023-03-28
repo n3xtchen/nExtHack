f3d6aae75ad562656b1aa2eac62726bcab8d78ce
n3xtchen <echenwen@gmail.com>
2023-03-28 17:37:14 +0800

    sql analytics gpt: 上传notebook

---
+++ sql-analytics-gpt/analytics_gpt.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2023 n3xtchen <echenwen@gmail.com>
+#
+# Distributed under terms of the GPL-2.0 license.
+
+"""
+
+"""
+
+class GPT:
+
+    def __init__(self, appkey):
+        pass
+
+    def ask(self, question):
+        pass
+
+    def answear(self):
+        pass
+
+
+class SQLGPT:
+
+    def gen_sql(self):
+        pass
+    def debug_sql(self):
+        pass
