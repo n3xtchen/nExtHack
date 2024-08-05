885f4bc467f5b6e8385a62dfd57089364f4e461b
n3xtchen <echenwen@gmail.com>
2024-08-05 19:52:22 +0800

    dns-updater: python项目化

--- dns-updater/dns_updater/AliApi.py
+++ dns-updater/dns_updater/AliApi.py
@@ -10,16 +10,17 @@
 """
 
 import json
-import tomllib
-from typing import ParamSpecArgs
-import sys
 
-from aliyunsdkcore.client import AcsClient
+from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import (
+    AddDomainRecordRequest, )
+from aliyunsdkalidns.request.v20150109.DescribeSubDomainRecordsRequest import (
+    DescribeSubDomainRecordsRequest, )
+from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import (
+    UpdateDomainRecordRequest, )
 from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
-from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
-from aliyunsdkalidns.request.v20150109.DescribeSubDomainRecordsRequest import DescribeSubDomainRecordsRequest
-from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
+from aliyunsdkcore.client import AcsClient
+
 
 class AliApi:
 
@@ -34,7 +35,8 @@ class AliApi:
 
         if self._client:
             return self._client
-        self._client = AcsClient(self.access_key, self.acces_secret, 'cn-hangzhou')
+        self._client = AcsClient(self.access_key, self.acces_secret,
+                                 'cn-hangzhou')
         return self._client
 
     def add_subdomian_record(self, sub_domain, ip):
@@ -43,18 +45,18 @@ class AliApi:
         request.set_accept_format('json')
         request.set_Value(ip)
         request.set_Type("A")
-        request.set_RR(www)
+        request.set_RR("www")
         request.set_DomainName(sub_domain)
-        response = self.client.do_action_with_exception(request)
-        print(str(response, encoding='utf-8')) 
+        response: str = self.client.do_action_with_exception(request)
+        print(str(response, encoding='utf-8'))
 
     def describe_subdomain_record(self, sub_domain):
         """获取记录"""
         request = DescribeSubDomainRecordsRequest()
         request.set_accept_format('json')
         request.set_SubDomain(sub_domain)
-        response = self.client.do_action_with_exception(request)
-        print(str(response, encoding='utf-8')) 
+        response: str = self.client.do_action_with_exception(request)
+        print(str(response, encoding='utf-8'))
         record = json.loads(response)["DomainRecords"]["Record"][0]
         return record["RR"], record["Value"], record["RecordId"]
 
@@ -71,20 +73,6 @@ class AliApi:
             request.set_RR(rr)
             request.set_Type("A")
             request.set_Value(new_ip)
-            response = self.client.do_action_with_exception(request)
-            print(str(response, encoding='utf-8')) 
+            response: str = self.client.do_action_with_exception(request)
+            print(str(response, encoding='utf-8'))
 
-if __name__ == "__main__":
-
-    # 读取 toml 文件
-    with open('config.toml', 'rb') as f:
-        config = tomllib.load(f)
-        print(config)
-
-        ali = AliApi(config["access_key"], config["access_secret"])
-        ali.describe_subdomain_record(config["sub_domain"])
-        if len(sys.argv) == 2:
-            ali.update_subdomain_record(config["sub_domain"], sys.argv[1])
-            ali.describe_subdomain_record(config["sub_domain"])
-        else:
-            print("参数异常", sys.argv)
